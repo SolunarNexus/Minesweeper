6702d2bd3715ad37c596fd2758babcc49b00bc58
Peter Stanko <peter.stanko0@gmail.com>
2022-04-12 08:28:06 +0200

    Add import/export functionality

--- src/main/java/cz/muni/fi/pv168/minesweeper/Board.java
+++ src/main/java/cz/muni/fi/pv168/minesweeper/Board.java
@@ -1,6 +1,8 @@
 package cz.muni.fi.pv168.minesweeper;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Random;
 import java.util.random.RandomGenerator;
@@ -32,6 +34,39 @@ public final class Board {
         this.rows = rows;
         this.cols = cols;
         this.mines = mines;
+    }
+
+
+    public static Board importBoard(String base64content) {
+        var content = new String(Base64.getDecoder().decode(base64content), StandardCharsets.UTF_8)
+                .split("\n");
+        var rowColsStr = content[0].split(",");
+        int rows = Integer.parseInt(rowColsStr[0]), cols = Integer.parseInt(rowColsStr[1]);
+
+        var board = new Board(rows, cols, content.length - 1);
+        board.cells = new ArrayList<BoardCell>(rows * cols);
+
+        for (int i = 0; i < rows * cols; i++) {
+            board.cells.add(new BoardCell());
+        }
+
+        for (int i = 1; i < content.length; i++) {
+            var rowColStr = content[i].split(",");
+            int row = Integer.parseInt(rowColStr[0]), col = Integer.parseInt(rowColStr[1]);
+            board.getCell(row, col).value = 'M';
+        }
+
+        // calculate numbers
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                char adjMinesChar = board.getAdjMinesCount(r, c);
+                if (adjMinesChar != 'M') {
+                    board.getCell(r, c).value = adjMinesChar;
+                }
+            }
+        }
+
+        return board;
     }
 
     public BoardCell getCell(int r, int c) {
@@ -90,9 +125,26 @@ public final class Board {
         }
     }
 
+
+    public String exportBoard() {
+        checkTooSoon();
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%d,%d\n", rows, cols));
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                if (getCell(r, c).value == 'M')
+                    sb.append(String.format("%d,%d\n", r, c));
+
+        return Base64
+                .getEncoder()
+                .withoutPadding()
+                .encodeToString(sb.toString().getBytes(StandardCharsets.UTF_8));
+    }
+
     private void generateRandomBoard(int selectedRow, int selectedCol) {
         cells = new ArrayList<BoardCell>(this.rows * this.cols);
-        for(int i = 0; i < rows * cols; i++) {
+        for (int i = 0; i < rows * cols; i++) {
             cells.add(new BoardCell());
         }
         RandomGenerator random = new Random();
@@ -124,9 +176,9 @@ public final class Board {
     private char getAdjMinesCount(int r, int c) {
         int counter = 0;
         if (getCell(r, c).value != 'M') {
-            for (var direction : DIRECTIONS) {
-                var row = direction[0] + r;
-                var col = direction[1] + c;
+            for (var dir : DIRECTIONS) {
+                var row = dir[0] + r;
+                var col = dir[1] + c;
 
                 try {
                     checkBounds(row, col);
@@ -163,7 +215,7 @@ public final class Board {
 
     @Override
     public String toString() {
-        if(cells == null) {
+        if (cells == null) {
             return "Not initialized!";
         }
 
--- src/main/java/cz/muni/fi/pv168/minesweeper/Minesweeper.java
+++ src/main/java/cz/muni/fi/pv168/minesweeper/Minesweeper.java
@@ -39,18 +39,34 @@ public final class Minesweeper {
                     doExit();
                 } else if ("debug".equalsIgnoreCase(parts[0]) || "d".equalsIgnoreCase(parts[0])) {
                     doDebug(board);
+                } else if ("export".equalsIgnoreCase(parts[0])) {
+                    doExport();
+                }
+            } else if (parts.length == 2) {
+                if ("import".equalsIgnoreCase(parts[0])) {
+                    doImport(parts[1]);
                 }
             } else if (parts.length == 3 && "reveal".equalsIgnoreCase(parts[0]) || "r".equalsIgnoreCase(parts[0])) {
                 if (!doReveal(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]))) {
                     handle_MINE(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+                    if (board.isCleared()) {
+                        doWon();
+                    }
                 }
             } else {
                 handle_ERROR("Invalid command: " + inputLine);
             }
-            if (board.isCleared()) {
-                doWon();
-            }
         }
+    }
+
+    private static void doExport() {
+        System.out.println(board.exportBoard());
+    }
+
+    private static void doImport(String content) {
+        board = Board.importBoard(content);
+        System.out.println("Board imported!");
+        doPrintBoard();
     }
 
     private static void doWon() {
@@ -81,6 +97,7 @@ public final class Minesweeper {
 
     private static void handle_MINE(int fst, int snd) {
         System.out.printf("Found mine @ coordinates [%d, %d]\n", fst, snd);
+        System.out.println("\n*** You lost!***\n");
         System.exit(1);
     }
 
