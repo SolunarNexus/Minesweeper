ba030b47346250ff203f5a1839a2b05b4c49ff1a
Paul Hrdina <qhrdina@fi.muni.cz>
2022-04-26 23:35:53 +0200

    Moving TestUtils.loadFromString() to BoardSoftAssertions and eliminating the resulting empty utility class

--- src/test/java/cz/muni/fi/pv260/minesweeper/BoardSoftAssertions.java
+++ src/test/java/cz/muni/fi/pv260/minesweeper/BoardSoftAssertions.java
@@ -2,16 +2,59 @@ package cz.muni.fi.pv260.minesweeper;
 
 import org.assertj.core.api.AutoCloseableSoftAssertions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 final class BoardSoftAssertions extends AutoCloseableSoftAssertions {
 
     private final Board board;
 
     BoardSoftAssertions(String boardAsString) {
-        this(TestUtils.loadFromString(boardAsString));
+        this(loadFromString(boardAsString));
     }
 
     BoardSoftAssertions(Board board) {
         this.board = board;
+    }
+
+    /**
+     * Loads a board from provided string
+     * each line represents a row, each char in row represents a column
+     * <p>
+     * Possible chars for cols:
+     * - 'M' - represents a mine
+     * - '.' - represents a cell that is revealed (and it is not a mine)
+     * - 'X' - represents an unrevealed mine
+     * FUTURE:
+     * - 'F' - represents a correct flag on the cell, cell is flagged and it is mine
+     * - 'W` - wrong flag - the flagged cell is not a mine!
+     *
+     * @param raw board representation
+     * @return initialized board
+     */
+    static Board loadFromString(String raw) {
+        var lines = raw.split("\n");
+        var rows = lines.length;
+        var cols = lines[0].trim().length();
+
+        List<BoardCell> cells = new ArrayList<>(rows * cols);
+
+        for (var line : lines) {
+            for (char ch : line.toUpperCase().toCharArray()) {
+                var cell = new BoardCell();
+                cells.add(cell);
+
+                switch (ch) {
+                    case 'M' -> cell.value = 'M';
+                    case '.' -> cell.isRevealed = true;
+                    case 'X' -> cell.isRevealed = false;
+                    case 'W', 'F' -> throw new UnsupportedOperationException("Not implemented yet");
+                    default -> throw new IllegalArgumentException("Unsupported character: " + ch);
+                }
+            }
+        }
+
+        return new Board(rows, cols, cells);
     }
 
     void assertRows(int expectedRows) {
